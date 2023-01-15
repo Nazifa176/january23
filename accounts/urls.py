2fcf051a136d2ddfc098b71d5ed573adefc0b574
Mehedi Hasan <mehedinahid27@gmail.com>
2023-01-15 11:46:43 +0600

    modified accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,13 @@ from .views import *
 app_name="accounts"
 
 urlpatterns=[
-    path("login/",LoginView.as_view(),name="login"),
-    path("register/",RegisterView.as_view(),name="register"),
+    path("financial-year/",FinancialYearView.as_view(),name="financial-year"),
+    path("sub-accounts/",SubAccountView.as_view(),name="sub-account"),
+    path("predifiend-accounts/",PredifiendAccountView.as_view(),name="predifind-account"),
+    path("credit-voucher/",CrebitVoucherView.as_view(),name="credit-voucher"),
+    path("debit-voucher/",DebitVoucherView.as_view(),name="debit-voucher"),
+    path("contra-voucher/",ContraView.as_view(),name="contra-voucher"),
+    path("opening-balance/",OpeningBalanceView.as_view(),name="opening-balance"),
+    path("journal-voucher/",JournalVoucherView.as_view(),name="journal-voucher"),
+    path("bank-reconcilation/",BankReconcilationView.as_view(),name="bank-reconcilation"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,38 @@ from django.shortcuts import render
 from django.views import View 
 
 
-class LoginView(View):
+class FinancialYearView(View):
     def get(self,request):
-        return render(request,"accounts/login.html")
-    
-    
-class RegisterView(View):
+        return render(request,"accounts/financial_year.html")
+
+class SubAccountView(View):
     def get(self,request):
-        return render(request,"accounts/register.html")
-    
+        return render(request,"accounts/sub_account.html")
+
+class PredifiendAccountView(View):
+    def get(self,request):
+        return render(request,"accounts/predefined_account.html")
+
+class DebitVoucherView(View):
+    def get(self,request):
+        return render(request,"accounts/debit_voucher.html")
+
+class CrebitVoucherView(View):
+    def get(self,request):
+        return render(request,"accounts/credit_voucher.html")
+
+class ContraView(View):
+    def get(self,request):
+        return render(request,"accounts/contra_voucher.html")
+class JournalVoucherView(View):
+    def get(self,request):
+        return render(request,"accounts/journal_voucher.html")
+
+class BankReconcilationView(View):
+    def get(self,request):
+        return render(request,"accounts/bank_reconcilation.html")
+
+class OpeningBalanceView(View):
+    def get(self,request):
+        return render(request,"accounts/opening_balance.html")
+
