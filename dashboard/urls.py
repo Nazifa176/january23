f358ae77009ee0a66c2fc118f0a8d809d159fb52
Mehedi Hasan <mehedinahid27@gmail.com>
2023-01-16 11:19:46 +0600

    added settings all html pages

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,5 +6,33 @@ app_name="dashboard"
 
 urlpatterns=[
     path("",DashBoardView.as_view(),name="dashboard"),
-    path("settings/",SettingsView.as_view(),name="settings")
+    path("add-banner-view/",AddBannerAreaView.as_view(),name="add-banner-view"),
+    path("about-us/",AboutUsView.as_view(),name="about-us"),
+    path("advertisment/",AdvertismentView.as_view(),name="advertisment"),
+    path("bid-settings/",BidSettingView.as_view(),name="bid-settings"),
+    path("home-page-section/",HomePageSectionView.as_view(),name="home-page-section"),
+    path("language-setting/",LanguageSettingView.as_view(),name="language-setting"),
+    path("manage-cupon/",ManageCuponView.as_view(),name="manage-cupon"),
+    path("payment-gateway/",PaymentGatwayView.as_view(),name="payment-gateway"),
+    path("privacy-policy/",PrivacyPolicyView.as_view(),name="privacy-policy"),
+    path("product-review/",ProductReviewView.as_view(),name="product-review"),
+    path("quality-setting/",QualitySettingView.as_view(),name="quality-setting"),
+    path("seo-setting/",SEOSettingView.as_view(),name="seo-setting"),
+    path("shipping-method/",ShippingMethodView.as_view(),name="shipping-method"),
+    path("slider-setting/",SliderSettingView.as_view(),name="slider-setting"),
+
+    path("sms-config/",SMSConfigView.as_view(),name="sms-config"),
+    path("social-media/",SocialMediaView.as_view(),name="social-media"),
+    path("subscriber/",SubscriberView.as_view(),name="subscriber"),
+
+
+
+
+
+
+    
+
+
+   
+    
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -6,7 +6,71 @@ class DashBoardView(View):
     def get(self,request):
         return render(request,"dashboard/dashboard.html")
   
-class SettingsView(View):
+class SliderSettingView(View):
     def get(self,request):
-        return render(request,"dashboard/settings.html")
-    
+        return render(request,"dashboard/slider_setting.html")
+
+class AdvertismentView(View):
+    def get(self,request):
+        return render(request,"dashboard/advertisment.html")
+
+class AddBannerAreaView(View):
+    def get(self,request):
+        return render(request,"dashboard/add_banner_area.html")
+
+class HomePageSectionView(View):
+    def get(self,request):
+        return render(request,"dashboard/home_page_section.html")
+
+class ProductReviewView(View):
+    def get(self,request):
+        return render(request,"dashboard/product_review.html")
+
+class SubscriberView(View):
+    def get(self,request):
+        return render(request,"dashboard/subscriber.html")
+
+class BidSettingView(View):
+    def get(self,request):
+        return render(request,"dashboard/bids_settings.html")
+
+class QualitySettingView(View):
+    def get(self,request):
+        return render(request,"dashboard/quality_setting.html")
+
+class ShippingMethodView(View):
+    def get(self,request):
+        return render(request,"dashboard/shipping_method.html")
+
+class SEOSettingView(View):
+    def get(self,request):
+        return render(request,"dashboard/seo_settings.html")
+
+class SocialMediaView(View):
+    def get(self,request):
+        return render(request,"dashboard/social_media.html")
+
+class AboutUsView(View):
+    def get(self,request):
+        return render(request,"dashboard/about_us.html")
+
+class PrivacyPolicyView(View):
+    def get(self,request):
+        return render(request,"dashboard/privacy_policy.html")
+
+class ManageCuponView(View):
+    def get(self,request):
+        return render(request,"dashboard/manage_cupon.html")
+
+class LanguageSettingView(View):
+    def get(self,request):
+        return render(request,"dashboard/language_setting.html")
+
+class SMSConfigView(View):
+    def get(self,request):
+        return render(request,"dashboard/sms_config.html")
+        
+class PaymentGatwayView(View):
+    def get(self,request):
+        return render(request,"dashboard/payment_gateway.html")
+  
